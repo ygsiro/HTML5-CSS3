678998b270b0d1c1242a34c06d77659c5ecc19d7
ygsiro <entoyukari@gmail.com>
2023-11-18 18:49:00 +0900

    docstring

--- node.py
+++ node.py
@@ -2,6 +2,12 @@ class Node:
     def __init__(
         self, name: str, id: str | None = None, class_: str | None = None, **kwargs
     ):
+        """
+        name: タグ名。半角英字のみ推奨、空白禁止、名前空間は考慮しない
+        id: ID. 半角英字のみ推奨。
+        class_: クラス。引数で指定する場合classが使用できないため_付
+        kwargs: その他属性
+        """
         self.__name = name
         self.__attr = kwargs
         if id:
@@ -13,12 +19,23 @@ class Node:
         self.__parent = None
 
     def __setitem__(self, key: str, value: str):
+        """
+        key: 属性名
+        value: 属性の値
+        """
         self.__attr[key] = value
 
     def __getitem__(self, key: str) -> str:
+        """
+        key: 属性名
+        return: 属性の値
+        """
         return self.__attr.get(key, "")
 
     def html_string(self, lank: int = 0) -> str:
+        """
+        HTML形式で表現
+        """
         result = lank * "  " + f"<{self.__name}"
         for k, v in self.__attr.items():
             result += f' {k}="{v}"'
@@ -32,41 +49,72 @@ class Node:
         return result
 
     def append(self, node):
+        """
+        子要素の追加
+        Nodeを継承したクラスしか追加できません
+        """
         if issubclass(type(node), Node):
             node.__root = self.__root
             node.__parent = self
             self.__child.append(node)
 
     def getId(self) -> str:
+        """
+        IDの取得
+        """
         return self.__attr.get("id", "")
 
     def getClass(self) -> list[str]:
+        """
+        CLASSリストの取得
+        """
         return self.__attr.get("class", "").split()
 
 
 class TextNode(Node):
     def __init__(self, text: str):
+        """
+        text: テキスト
+        """
         super().__init__("text")
         self.text = text
         self.__root = self
         self.__parent = None
 
     def __setitem__(self, key: str, value: str):
+        """
+        属性はなし
+        """
         pass
 
     def __getitem__(self, key: str) -> str:
+        """
+        属性はなし
+        """
         return ""
 
     def html_string(self, lank: int = 0) -> str:
+        """
+        文字を返す
+        """
         return lank * "  " + f"{self.text}\n"
 
     def getId(self) -> str:
+        """
+        IDは無し
+        """
         return ""
 
     def getClass(self) -> list[str]:
+        """
+        CLASSは無し
+        """
         return []
 
     def append(self, node):
+        """
+        子要素は追加できない
+        """
         pass
 
 
