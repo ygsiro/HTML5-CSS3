ba718bfbf59d4068678c7b1e4686c43ca1787faa
ygsiro <entoyukari@gmail.com>
2023-12-25 20:57:20 +0900

    run

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import json
 
 with open("list.json") as a_f:
-    elements = json.load(a_f)
-    for key, value in elements.items():
-        print(f'<li><a href="{key}.html">{key}</a></li>')
+    elements: dict = json.load(a_f)
+    test = list(elements.keys())
+    test.sort()
+    for i in test:
+        print(i)
